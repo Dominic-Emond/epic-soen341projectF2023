59eb61520edec9dea857b2123b80c05e0668d1df
Dominic-Emond <emond.dominic@gmail.com>
2023-11-09 10:01:49 -0500

    Added an SQL template to add new values in the database
    This is a temporary step until it can be done from the frontend

---
+++ src/database/InsertValues-Template.sql
@@ -0,0 +1,19 @@
+use soen341;
+
+/*
+Insert into Broker (First_Name, Last_Name, Email_Address, Username, Pass)
+Values ('Broker', 'LastName', 'Test@test.com', 'Test', 'Test');
+
+SELECT * FROM Broker */
+
+/*
+INSERT INTO Property (Broker_Id, Address, Price)
+Values (1, 'Test Address', 100);
+
+SELECT * FROM Property; */
+
+/*
+INSERT INTO Client (First_Name, Last_Name, Type, Username, Pass)
+Values ('Test', 'Test', 1, 'usertest', 'passtest');
+
+Select * from Client; */
