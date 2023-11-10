9516cb27e11348c06c6e237928b8cd41a569f5f3
GianlucaGirardi <gigirar@DESKTOP-NC06C6F>
2023-11-10 16:49:31 -0500

    updated script to obtain form information from sign in page and sput in JSON

--- src/frontEnd/script.js
+++ src/frontEnd/script.js
@@ -15,6 +15,8 @@
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
         const confirmPassword = document.getElementById("confirmPassword").value;
+        const isBroker = document.getElementById("broker").value;
+        const isClient = document.getElementById("client").value;
 
         // Validate input fields here (e.g., check if passwords match, email format, etc.)
 
@@ -24,6 +26,8 @@
             email,
             username,
             password,
+            isBroker,
+            isClient
         };
 
         // Convert the JavaScript object to a JSON string
@@ -33,7 +37,54 @@
         console.log("JSON Data to Send to Backend:", jsonData);
 
          // Make an HTTP POST request to your local API
-         fetch("http://localhost:8080/api/create-account", {
+         fetch("http://localhost:8000/api/create-account", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: jsonData,
+        })
+        .then((response) => {
+            if (response.ok) {
+                // Handle a successful response (e.g., show a success message)
+                alert("Account created successfully!");
+            } else {
+                // Handle errors (e.g., display an error message)
+                alert("Failed to create an account. Please try again.");
+            }
+        })
+        .catch((error) => {
+            // Handle network or other errors
+            alert("An error occurred. Please try again later.");
+            console.error(error);
+        });
+    });
+});
+
+//Code for Sign In
+document.addEventListener("DOMContentLoaded", function () {
+    const registrationForm = document.getElementById("logInForm");
+    const createAccountButton = document.getElementById("signInButton");
+
+    createAccountButton.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        // Create a JavaScript object to represent the user data
+        const signInData= {
+            username,
+            password,
+        };
+
+        // Convert the JavaScript object to a JSON string
+        const jsonData = JSON.stringify(signInData);
+
+        // Simulate sending the JSON data to the backend API
+        console.log("JSON Data to Send to Backend:", jsonData);
+
+         // Make an HTTP POST request to your local API
+         fetch("http://localhost:8000/api/create-account", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
