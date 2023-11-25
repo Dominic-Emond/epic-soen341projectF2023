6aa6917baaba7c877d400828f889d45ebae71c8d
Dominic-Emond <emond.dominic@gmail.com>
2023-11-25 02:35:07 -0500

    Removing special Github output from the pytest file

--- src/api/test_broker.py
+++ src/api/test_broker.py
@@ -7,16 +7,7 @@ connectAndRoute()
 
 # Testing Broker API Endpoints
 def test_broker():
-
-    # Checking if the routes has properly there!
-    try: # Github
-        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-            print(str(app.routes), file=fh)
-    except: # Local
-        print(client.base_url)
-        print(app.routes)
-        pass
-
+    
     # Post
     sample_broker = {
         "First_Name": "Harry",
