5f0b4ef2971503dc38d57c97e3e7d724053b67c0
Ayoub <www.akchaou@gmail.com>
2023-11-26 22:45:10 -0500

    fixing test offer

--- src/database/Sprint4-AddingOfferTable.sql
+++ src/database/Sprint4-AddingOfferTable.sql
@@ -19,7 +19,8 @@ Create TABLE Offer (
 
 Alter TABLE Offer
 ADD Constraint fk_offer_property FOREIGN KEY (PropertyId) References Property(Id),
-ADD Constraint fk_offer_client FOREIGN KEY (ClientId) References Client(Id);
+ADD Constraint fk_offer_client FOREIGN KEY (ClientId) References Client(Id),
+ADD Constraint fk_offer_broker FOREIGN KEY (BrokerId) References Broker(Id);
 
 # Adding New Table Favourites for Client Favourites Management
 CREATE TABLE Favourite (
